5f8759f73218fff8a0a8173ddaf9ebdb0109da20
RokitDan <dleeschell@gmail.com>
2022-07-15 18:09:17 -0400

    Created Factorial App

--- js/site.js
+++ js/site.js
@@ -1,5 +1,30 @@
-function getValue() {
-  alert("BEAN");
+function getNumbers() {
+  let startValue = document.getElementById("startValue").value;
+  let endValue = document.getElementById("endValue").value;
+
+  //parse the numbers
+  startValue = parseInt(startValue);
+  endValue = parseInt(endValue);
+
+  let outNumber = 0;
+
+  if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+    // true condition
+    for (let i = startValue; i <= endValue; i++) {
+      outNumber += i;  
+      
+    }
+
+    document.getElementById("results").innerHTML = outNumber;
+
+  }else{
+    // false condition
+    alert("Please enter a number");
+
+  }
+
+
+
 }
 
 function getValue2() {
